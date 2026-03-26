5dfaac9191b422366938772c62484ea49a2a0fbe
vishalgupta-2828 <25113cn085@glbitm.ac.in>
2026-03-26 22:07:13 +0530

    Add files via upload
    largest number

---
+++ largestnumber.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int a, b, c;
+    printf("enter three numbers ");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a>b)
+    {
+        if(a>c)
+        printf("%d is the largest",a);
+    
+    else
+    
+        printf("%d is the largest",c);
+    }
+    else
+    {
+        if(b>c)
+        {
+            printf("%d is the largest",b);
+        }
+       else
+        {
+            printf("%d is the largest",c);
+        } 
+    }
+    return 0;
+}
